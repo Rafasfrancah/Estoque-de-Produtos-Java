eca7cf0997a4785662543846fcb8ac18eaf695bc
Rafael Franca <rafasfranca@gmail.com>
2022-11-13 08:53:16 -0300

    Melhoria no sistema. (Adicionei construtores)

--- SistemaDeEstoque/src/entities/Product.java
+++ SistemaDeEstoque/src/entities/Product.java
@@ -1,9 +1,17 @@
-package entities;
+ package entities;
 
 public class Product {
 	public String name;
 	public double price;
 	public int quantity;
+	
+	public Product(String name, double price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+		
+	}
+	
 
 	public double totalValueInStock() {
 		return price * quantity;
